ac1a1819fa12b9beb710ee265b426e68ba16d79c
k.behrouzi <katibehrouzi@gmail.com>
2025-07-22 15:04:38 +0330

    dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import styles from './dashboard.module.scss';
+
+const DashboardPage = () => {
+
+  return (
+    <h1 className={styles.container}>
+        Welcome to the dashboard 🎉
+    </h1>
+  );
+}
+
+export default DashboardPage
