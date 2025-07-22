154b01397d9f9c678a62b6fb9accd1ad9802258e
k.behrouzi <katibehrouzi@gmail.com>
2025-07-22 15:16:00 +0330

    authentication with save in local storage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+import { useEffect } from 'react';
 import styles from './auth.module.scss';
 import AuthForm from "./AuthForm";
 
 
 const AuthPage = () => {
-
+    useEffect(() => {
+        localStorage.removeItem('user');
+    }, []);
     return (
         <div className={styles.container}>
             <h1>ورود</h1>
